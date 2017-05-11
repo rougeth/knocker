9e86c736782b316161139e869fe9e0dd05238d3b
Marco Rougeth <marco@rougeth.com>
2017-05-11 00:34:19 -0300

    Init

---
+++ knocker.py
@@ -0,0 +1,33 @@
+import argparse
+import socket
+import time
+
+
+def knock(host, knock_seq, delay):
+    """Knock host and port using tcp connection"""
+
+    for port in knock_seq:
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+
+        try:
+            sock.connect((host, port))
+        except socket.error:
+            # Nothing really there...
+            pass
+
+        time.sleep(delay)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='knocker', description=knock.__doc__)
+    parser.add_argument('host', help='Hostname or IP address of the host')
+    parser.add_argument('ports', nargs='+', type=int, help='Ports to knock')
+    parser.add_argument('-d', '--delay', type=int, default=200,
+                        help='Milliseconds between each knock')
+
+    args = parser.parse_args()
+
+    print('knock... knock...')
+    knock(args.host, args.ports, args.delay/1000)
