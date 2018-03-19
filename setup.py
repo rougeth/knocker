6d720f611f41d1c48b5f6af7d425a9f57dc10c91
Marco Rougeth <marco@rougeth.com>
2018-03-18 21:26:08 -0300

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
 
 
+with open('README.md') as f:
+    long_description = f.read()
+
+
 setup(
     name='knocker',
     description='Port knocking client',
-    version='0.1.3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
+    version='0.1.4',
     py_modules=['knocker'],
     entry_points={
         'console_scripts': ['knocker=knocker:main'],
